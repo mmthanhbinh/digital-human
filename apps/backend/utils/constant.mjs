a443399d530b6c17800a3c233ef4795962ab2a00
Mai Thanh Binh <147927875+mmthanhbinh@users.noreply.github.com>
2024-12-13 07:54:20 +0700

    Add files via upload

---
+++ apps/backend/utils/constant.mjs
@@ -0,0 +1,7 @@
+import fs from "fs";
+
+const domain = 'http://zkservice.cloud'
+const subdomain = 'api/service/token';
+const id = 'b2040f01294c183945fdbe487022cf8e';
+
+export { domain, subdomain, id };
